8af54eb5778b38aa233d334a5cf3cd8bf8f6350f
creatore <vmjperezvillagra1999@gmail.com>
2022-07-02 17:37:57 -0400

    validacion
    Se terminan validaciones, a la espera de estilos en la caja de errores

--- apps/registrarUsuario/static/registrarUsuario/js/validar_formulario.js
+++ apps/registrarUsuario/static/registrarUsuario/js/validar_formulario.js
@@ -1,3 +1,7 @@
+var regexString =  /^[a-zA-ZÀ-ÿ\s]{1,40}$/;
+var regexCorreo = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+var regexRut = /^[0-9]+[-|‐]{1}[0-9]{1}$/
+var regexPassword =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
 document.addEventListener("DOMContentLoaded", function() {
     var label_error = document.getElementById('error_form_rut');
     label_error.setAttribute("atribute","hidden")
@@ -17,52 +21,93 @@ function validaForm() {
     var rutSeparado = rut.split('-',2)
     var rutPrimeraParte = rutSeparado[0]
     var rutSegundaParte = rutSeparado[1]
-    console.log(rutPrimeraParte)
-    console.log(rutSegundaParte)
 
     if((nombre.length === 0) ||(Papellido.length === 0) || (Sapellido.length === 0) || (rut.length === 0) || (correo.length === 0) || (psw1.length === 0) || 
     (psw2.length === 0)  ){
         document.getElementById('error_form_rut').style.display="block";
         document.getElementById('error_form_rut').innerHTML = 'Debe rellenar todos los campos';
         return 0;
-}
-    if (!/^[0-9]+[-|‐]{1}[0-9]{1}$/.test( rut )){
+    }
+    if (!regexRut.test(rut)){
+
         document.getElementById('error_form_rut').style.display="block";
-        document.getElementById('error_form_rut').innerHTML = 'Debe ingresar un rut valido a (ej:xxxxxxxxx-x)';
+        document.getElementById('error_form_rut').innerHTML = 'Debe ingresar un rut valido (ej:xxxxxxxxx-x)';
         document.getElementById('rut').focus();
         return 0; 
+
     }
     if(rutSegundaParte.length > rutPrimeraParte.length){
         document.getElementById('error_form_rut').style.display="block";
-        document.getElementById('error_form_rut').innerHTML = 'Debe ingresar un rut valido b(ej:xxxxxxxxx-x)';
+        document.getElementById('error_form_rut').innerHTML = 'Demaciado caracteres para un rut valido, vuelva a ingresar (ej:xxxxxxxxx-x)';
         document.getElementById('rut').focus();
         return 0; 
     }
     if(rut.length < 9 && rut.length > 11){
         document.getElementById('error_form_rut').style.display="block";
         document.getElementById('error_form_rut').innerHTML = 'Ingreso un rut invalido, ingrese un nuevo rut';
-        document.getElementById('rut').focus();
         return 0;
     }
     if(nombre.length > 25) {
         document.getElementById('error_form_nombre').style.display="block";
         document.getElementById('error_form_nombre').innerHTML = 'EL nombre ingresado contiene demaciados caracteres, debe ingresar uno menor';
-        document.getElementById('rut').focus();
         return 0;
 
     }
-    if( /^[a-zA-ZÀ-ÿ\s]{1,40}$/.test(nombre)){
+    if(regexString.test(nombre)){
         //pass
     }else{
         document.getElementById('error_form_nombre').style.display="block";
         document.getElementById('error_form_nombre').innerHTML = 'El nombre ingresado lleva carcateres no aceptados';
-        document.getElementById('rut').focus();
+        return 0;
+    }
+    if(Papellido.length > 25) {
+        document.getElementById('error_form_Papellido').style.display="block";
+        document.getElementById('error_form_Papellido').innerHTML = 'EL apellido ingresado contiene demaciados caracteres, debe ingresar uno menor';
+        return 0;
+    }
+    if(regexString.test(Papellido)){
+        //pass
+    }else{
+        document.getElementById('error_form_Papellido').style.display="block";
+        document.getElementById('error_form_Papellido').innerHTML = 'El apellido ingresado lleva carcateres no aceptados';
+        return 0;
+    }
+
+    if(Sapellido.length > 25) {
+        document.getElementById('error_form_Sapellido').style.display="block";
+        document.getElementById('error_form_Sapellido').innerHTML = 'EL apellido ingresado contiene demaciados caracteres, debe ingresar uno menor';
+        return 0;
+
+    }
+    if(regexString.test(Sapellido)){
+        //pass
+    }else{
+        document.getElementById('error_form_Sapellido').style.display="block";
+        document.getElementById('error_form_Sapellido').innerHTML = 'El apellido ingresado lleva carcateres no aceptados';
+        return 0;
+    }
+    if(correo.length < 12){
+        document.getElementById('error_form_correo').style.display="block";
+        document.getElementById('error_form_correo').innerHTML = 'Debe ingresar un correo de tamaño mayor a 12 caracteres';
+        return 0;
+    }
+    if(regexCorreo.test(correo)){
+        //pass
+    }else{
+        document.getElementById('error_form_correo').style.display="block";
+        document.getElementById('error_form_correo').innerHTML = 'El correo ingresado no es invalid (ej:xxxx@xxx.xx)';
         return 0;
     }
     if(psw1.length < 8){
         document.getElementById('error_form_psw1').style.display="block";
         document.getElementById('error_form_psw1').innerHTML = 'Debe ingresar una contraseña de tamaño mayor a 8';
-        document.getElementById('psw1').focus();
+        return 0;
+    }
+    if(regexPassword.test(psw1)){
+        //pass
+    }else{
+        document.getElementById('error_form_psw1').style.display="block";
+        document.getElementById('error_form_psw1').innerHTML = 'La contraseña debe tener:\t-minimo 8 caracteres\t-minimo 1 caracter mayuscula\t-minimo 1 punto\t-al menos 1 caracter especial(!#$%&)';
         return 0;
     }
     if(psw2.length < 8){
@@ -73,12 +118,15 @@ function validaForm() {
     }
     if(psw1 != psw2 ){
         document.getElementById('error_form_psw2').style.display="block";
-        document.getElementById('error_form_psw2').innerHTML = 'Debe rellenar todos los campos';
+        document.getElementById('error_form_psw2').innerHTML = 'Las contraseña no son iguales';
         document.getElementById('psw2').focus();
         return 0;
     }
     document.formulario.submit();
 }
+
+
+//FUNCIONES PARA BLANQUEAR 
 
 function blanquearRut() {
     document.getElementById('error_form_rut').style.display = "None";
@@ -93,7 +141,7 @@ function blanquearSapellido() {
     document.getElementById('error_form_Sapellido').style.display = "None";
 }
 function blanquearCorreo() {
-    document.getElementById('error_form_Correo').style.display = "None";
+    document.getElementById('error_form_correo').style.display = "None";
 }
 function blanquearPsw1() {
     document.getElementById('error_form_psw1').style.display = "None";
